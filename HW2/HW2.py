5d5faf08ba0c3e382c68f31ea7d8816da10c3a96
Paul <kiripaul@gmail.com>
2014-01-30 10:06:44 -0500

    Dates work now!

--- HW2/HW2.py
+++ HW2/HW2.py
@@ -1,29 +1,44 @@
 import json
 import gzip
+import datetime
 from string import punctuation
 from  __builtin__ import any as nick_name
 
 def Load_Tweets():
     counter = 0
+    Obama_corpus = []
+    Romney_corpus = []
+    Obamney_corpus = []
+    Obama_date_corpus = []
+    Romney_date_corpus = []
+    Obamney_date_corpus = []
     for line in gzip.open('C:\\Users\\Paul\\Documents\\Spring 2014\\Data_Science_Visualization\\HomeWorks\\HW2\\HW02_twitterData.json.txt.gz','r'):
-        if counter >=5:
+        if counter >=10:
             break
         tweet = json.loads(line.strip())
         #print tweet["created_at"], type(tweet["text"]), tweet["text"]
         
         #removing punctuation and separating tweet into list of words
         clean_tweet = Sanitize_Tweet(tweet["text"])
+        #formatting the date to make it nice and useable
+        clean_date = Sanitize_Date(tweet["created_at"])
         
         #checking to see who is mentioned
         #if check_value = 0, obama ; check_value = 1, romney; check_value= 2, both
-        check_value = Sort_Out_Candidates(clean_tweet, tweet["created_at"])#checking to see which candidate the tweet is about
-        if check_value == 0:
-            print "ay"
-        elif check_value == 1:
-            print "ya"
-        else:
-            print "na"
+        check_value = Sort_Out_Candidates(clean_tweet)#checking to see which candidate the tweet is about
+        clean_tweet = list(clean_tweet)
         
+        #Appending each tweet to its respective corpus
+        if check_value == 0:#obama
+            Obama_corpus.append(clean_tweet)
+            Obama_date_corpus.append(clean_date)
+        elif check_value == 1:# Romney
+            Romney_corpus.append(clean_tweet)
+            Romney_date_corpus.append(clean_date)
+        else:# both
+            Obamney_corpus.append(clean_tweet)
+            Obamney_date_corpus.append(clean_date)
+
         #print tweet["text"]
         #print check_value
         #print clean_tweet
@@ -44,12 +59,19 @@ def Sanitize_Tweet(tweet):
     tweet_to_list = [word.lower() for word in tweet_to_list]
     return tweet_to_list
     
-def Sort_Out_Candidates(clean_tweet, created_at_date):
+def Sanitize_Date(tweet):
+    formatted_date_years = datetime.datetime.strptime(tweet,"%a, %d %b %Y %H:%M:%S" )
+    formatted_date_years = formatted_date_years.replace(minute=0,second=0)
+    #print tweet
+    return formatted_date_years
+    
+def Sort_Out_Candidates(clean_tweet):
+    # 
     # 0 : One of Obama's nicknames was found in tweet
     # 1 : One of Romney's nicknames was found in tweet
     # 2 : Both of their nickname were found in tweet
     
-    obama_names = ["obama","bama","barry","b.o","barrack","barack","bear","renegade","obomber","bammy","brak","divider"]
+    obama_names = ["obama","bama","barry","b.o","barrack","barack","bear","renegade","obomber","bammy","brak","divider","obamamama"]
     romney_names = ["mitt","romney","mittens","mormon","mitney","mit","waffle","binder","nobama","romnuts","javelin"]
     both_flag = [0,0] # if 1, then only romney, if 2 then both romeny and obama
     for name in romney_names:
