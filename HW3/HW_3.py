0aecb1369196ffe9d37076f0360c8aa5a0f4150e
Paul <kiripaul@gmail.com>
2014-02-04 19:17:10 -0500

    Pretty Printer Works
    Output looks nicer now and easier to read.

--- HW3/HW_3.py
+++ HW3/HW_3.py
@@ -1,14 +1,18 @@
 import json
+import pprint
 
 def Load_Messages():
     counter = 0
     for line in open('C:\\Users\\Paul\\Documents\\Spring 2014\\DSV_Data_Files\\HW_3\\messages.json.txt','r'):
         user_message = json.loads(line.strip())
-        if counter == 20:
+        if counter == 6:
             break
         else:
             #print user_message["sender"],user_message["message"]
-            print user_message["message_time"]
+            #print user_message["message_time"]
+            #print user_message
+            pp = pprint.PrettyPrinter(5)
+            pp.pprint(user_message)
             print ""
             counter +=1
             
