905ddd6d55db8725dfd0c17f9374dd6fb16b11c2
Paul <kiripaul@gmail.com>
2014-02-09 20:03:47 -0500

    Updated Date Grabber
    Grabs Dates correctly now.

--- HW3/HW_3-Gender_Age_SignUp.py
+++ HW3/HW_3-Gender_Age_SignUp.py
@@ -1,2 +1,16 @@
-for line in open('C:\\Users\\Paul\\Documents\\Spring 2014\\DSV_Data_Files\\HW_3\\bios.txt','r'):
-        user_message = (line)
+import csv
+counter = 0
+tstamps =[]
+tstamps2=[]
+
+with open ('C:\\Users\\Paul\\Documents\\Spring 2014\\DSV_Data_Files\\HW_3\\bios.csv','r') as doc:
+    for line in csv.DictReader(doc):
+        tstamps2.append(line["signup_date"])
+        tstamps.append(line["signup_date"][:10])
+        if counter == 20:
+            break
+        counter +=1
+
+print tstamps
+print ""
+#print tstamps2
