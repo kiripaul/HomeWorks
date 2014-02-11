c818ffcdf3b452ea6616967a1b3e68dfc7323d73
Paul <kiripaul@gmail.com>
2014-02-10 23:40:01 -0500

    Done

--- HW3/HW_3-Gender_Age_SignUp.py
+++ HW3/HW_3-Gender_Age_SignUp.py
@@ -4,7 +4,6 @@ import datetime
 import time
 import matplotlib.dates as dates
 import matplotlib,matplotlib.pyplot as plt
-import collections
 import unicodedata
 from collections import Counter
 
@@ -14,24 +13,15 @@ gender=[]
 ages=[]
 user_message_dates=[]
 counter = 0
+mean_list={}
 
 def Graph_SignUp_Date(ts_formatted):
     dates_counted = Counter(tstamps_formatted)
     # sorted lists [ (key1,val1), (key2,val2), ... ]
     # must sort BEFORE we break the (k,v)-pairs apart!
     dates_sorted = sorted(dates_counted.items())
-    #print dates_counted
-    #print ""
-    #print dates_sorted
-    
     # [(key1,val1), (key2,val2), ... ] --> (key1,key2,...), (val1,val2,...)
-    dates_sort, ndates = zip(*dates_sorted)
-    
-    # replace the python date lists with matplotlib dates:
-    #dates_sort = matplotlib.dates.date2num(dates_sort)
-    
-    plt.hold(True)
-    
+    dates_sort, ndates = zip(*dates_sorted)    
     # use plot_date() not plot()
     plt.plot_date(dates_sort, ndates, 'c-') # blue line
     
@@ -43,11 +33,6 @@ def Graph_SignUp_Date(ts_formatted):
     # This is a little small in this notebook, so here's a way
     # to control the size of the figure:
     plt.gcf().set_size_inches(12,8) # gcf = "get current figure"
-    '''
-    z=N.polyfit(dates_sort,ndates,1)
-    p=N.poly1d(z)
-    plt.plot(dates_sort,p(dates_sort),'r--') #add trendline to plot
-    '''
     # Here's a trick to style the x-labels
     plt.gcf().autofmt_xdate() # takes some googling to discover these
     plt.show()
@@ -57,7 +42,6 @@ def Graph_Gender(gender):
     gender_sorted = sorted(gender_counted.items())
     # [(key1,val1), (key2,val2), ... ] --> (key1,key2,...), (val1,val2,...)
     gender_identification, gender_value = zip(*gender_sorted)
-    #print gender_value , gender_identification
     #converting gender value and id to list
     gender_value = list(gender_value)
     gender_identification = list(gender_identification)
@@ -74,24 +58,17 @@ def Graph_Gender(gender):
     gender_identification.pop(0)#need to remove first value
     for i in range(4):
         gender_identification.pop(2)
-        
-    print gender_identification    
+           
     gender_identification.insert(0,0)
     gender_identification[1] = 1
     gender_identification[2] = 2
     
     for i in range(3):
         gender_identification[i] = int(gender_identification[i])
-    '''
-    print ""    
-    print gender_value , gender_identification 
-    print type(gender_value[0]), type(gender_identification[2])
-    '''
+
     Labels = ["Unknown","Female","Male"]
-    #plt.hold(True)
     plt.bar(gender_identification, gender_value, align="center") # blue line
     plt.xticks(range(len(gender_value)),Labels)
-    
     
     # annotate the plot:
     plt.xlabel("Gender Identification", fontsize=18)
@@ -106,17 +83,12 @@ def Graph_Gender(gender):
     
 def Graph_Age(age):
     age_counted = Counter(age)
-    #print age_counted
-    #print ""
     age_sorted = sorted(age_counted.items())
     # [(key1,val1), (key2,val2), ... ] --> (key1,key2,...), (val1,val2,...)
     age, age_count = zip(*age_sorted)
-    #print age_sorted
     
-    #plt.hold(True)
-    plt.bar(age, age_count, align="center") # blue line
-    #plt.xticks(range(len(gender_value)),Labels)
-    
+    plt.hold(True)
+    plt.bar(age, age_count, align="center")
     
     # annotate the plot:
     plt.xlabel("Ages of Users", fontsize=18)
@@ -132,62 +104,53 @@ def Fix_Time(mTime):
     #converting mTime to datetime data type   
     if (type(mTime)== float):
         mTime= time.strftime("%Y-%m-%d %H:%M:%S",time.localtime(mTime))
-        #mTime= time.strftime("%Y-%m-%d",time.localtime(mTime))
     
     mTime=datetime.datetime.strptime(mTime,"%Y-%m-%d %H:%M:%S").date()
-    #mTime=datetime.datetime.strptime(mTime,"%Y-%m-%d")    
-    #print mTime
     return mTime
     
 def Graph_Usage(message_dates):
     messages_counted = Counter(message_dates)
     messages_sorted = sorted(messages_counted.items())
-    print messages_sorted
     
     # [(key1,val1), (key2,val2), ... ] --> (key1,key2,...), (val1,val2,...)
     dates, date_count = zip(*messages_sorted)
-    #print age_sorted
     
-    #plt.hold(True)
-    plt.bar(dates, date_count, align="center") # blue line
-    #plt.xticks(range(len(gender_value)),Labels)
-    
+    plt.plot_date(dates,date_count,"bo")
     # annotate the plot:
-    plt.xlabel("Ages of Users", fontsize=18)
-    plt.ylabel("Number of Users", fontsize=18)
-    plt.title("Distribution of User Ages", fontsize=18)
+    plt.xlabel("Dates", fontsize=18)
+    plt.ylabel("Number of Posts on that Date", fontsize=18)
+    plt.title("Distribution of User Usage", fontsize=18)
     
     # This is a little small in this notebook, so here's a way
     # to control the size of the figure:
     plt.gcf().set_size_inches(12,8) # gcf = "get current figure"
+    plt.gcf().autofmt_xdate() # takes some googling to discover these
     plt.show()
+
+def Main():    
+    with open ('C:\\Users\\Paul\\Documents\\Spring 2014\\DSV_Data_Files\\HW_3\\bios.csv','r') as doc:
+        for line in csv.DictReader(doc):
+            #print type(line)
+            date = line["signup_date"][:19]
+            gender.append(line["gender"])
+            age = int(line["age"])
+            if age >= 8 and age <= 80:
+                ages.append(age)
+            tstamps.append(date) #grabbing dates when signedup
+            dt = dates.datestr2num(date)
+            tstamps_formatted.append(dt)
+            
+        #Graph_SignUp_Date(tstamps_formatted)
+        #Graph_Gender(gender)
+        #Graph_Age(ages)
     
-
-with open ('C:\\Users\\Paul\\Documents\\Spring 2014\\DSV_Data_Files\\HW_3\\bios.csv','r') as doc:
-    for line in csv.DictReader(doc):
-        #print type(line)
-        date = line["signup_date"][:19]
-        gender.append(line["gender"])
-        age = int(line["age"])
-        if age >= 8 and age <= 80:
-            ages.append(age)
-        tstamps.append(date) #grabbing dates when signedup
-        dt = dates.datestr2num(date)
-        tstamps_formatted.append(dt)
-        
-    #Graph_SignUp_Date(tstamps_formatted)
-    #Graph_Gender(gender)
-    #Graph_Age(ages)
-
-
-for line in open('C:\\Users\\Paul\\Documents\\Spring 2014\\DSV_Data_Files\\HW_3\\messages.json.txt','r'):
-    user_message = json.loads(line.strip())
-    #print type(user_message)
-    mTime=user_message["message_time"]
-    yTime = Fix_Time(mTime)
-    user_message_dates.append(yTime)
-
-Graph_Usage(user_message_dates)
+    for line in open('C:\\Users\\Paul\\Documents\\Spring 2014\\DSV_Data_Files\\HW_3\\messages.json.txt','r'):
+        user_message = json.loads(line.strip())
+        mTime=user_message["message_time"]
+        yTime = Fix_Time(mTime)
+        user_message_dates.append(yTime)
+    
+    Graph_Usage(user_message_dates)
     
 
     
